886e7f884b0cb8c9365a7ac3b4484c84478df94a
Hao Qian <haoqian0509@gmail.com>
2021-10-21 21:55:31 +1100

    use json-server to hosting backend

---
+++ src/components/EchartLine.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-chart class="chart" :option="lineData" autoresize/>
+    <!-- <div>
+        {{lineData}}
+    </div> -->
+</template>
+
+<script>
+
+import { GridComponent } from 'echarts/components';
+import { LineChart } from 'echarts/charts';
+import { UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import { use } from "echarts/core";
+import VChart from "vue-echarts";
+
+use([GridComponent, LineChart, CanvasRenderer, UniversalTransition]);
+
+export default {
+    name:"EchartLine", 
+    components: {
+        VChart,
+    },
+    
+    props: {
+        lineData: Object,
+    }, 
+}
+</script>
+
+<style>
+.chart {
+  height: 700;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/views/Reuse.vue
+++ src/views/Reuse.vue
@@ -1,34 +1,59 @@
 <template>
-  <v-container>
-    <v-row>
-      <template v-for="n in 2">
-        <v-col :key="n">
-          <v-card
-            class="pa-2"
-            outlined
-            tile
-            height="100px"
-          >
-            Column
-          </v-card>
-        </v-col>
-        <!-- <v-responsive
-          v-if="n === 2"
-          :key="`width-${n}`"
-          width="100%"
-        ></v-responsive> -->
-      </template>
-    </v-row>
-  </v-container>
+    <div>
+        <v-container>
+            <v-row>
+            <template v-for="n in 2">
+                <v-col :key="n">
+                <v-card
+                    outlined
+                    tile
+                    class="lines"
+                >
+                    <EchartLine :lineData="data[n-1]"/>
+                </v-card>
+                </v-col>
+            </template>
+            </v-row>
+        </v-container>
+    </div>
 </template>
 
 <script>
+import EchartLine from '../components/EchartLine.vue'
+
+
 export default {
     name: "Reuse",
+    components : {
+        EchartLine, 
+    }, 
+    data() {
+        return {
+            data : {}
+        }
+    },
+    methods : {
+        async fetchData() {
+            const res = await fetch(`http://localhost:5000/lineDATA`)
+            const data = await res.json()
+            console.log(data)
+            console.log(typeof(data))
+            return data
+        },
+    }, 
+
+    async created() {
+        this.data= await this.fetchData()
+    }
 
 }
 </script>
 
 <style>
+
+div.lines {
+    width: 550px;
+    height: 700px;
+}
 
 </style>
